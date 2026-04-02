e8ab92d801bce057f65ad14c880b4b58e90d1142
JhonveraDev <seguridad.veracontainer@gmail.com>
2026-04-01 21:15:29 -0500

    storeProvider Fixed

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,11 +2,14 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 
+import { StoreProvider } from '../features';
 import "../assets/styles/global.css";
 import "../assets/styles/reset.css";
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <App />
+    <StoreProvider>
+      <App />
+    </StoreProvider>
   </BrowserRouter>
 );
