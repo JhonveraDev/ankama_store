2ec33f1fa1fd32671294c6cdc481201b167765e0
JhonveraDev <seguridad.veracontainer@gmail.com>
2026-04-04 14:57:41 -0500

    new styles - Game Nav

--- src/shared/components/GameNav/GameNavItem.tsx
+++ src/shared/components/GameNav/GameNavItem.tsx
@@ -3,12 +3,12 @@ import { storeData } from "../../../shared";
 
 export const GameNavItem = () => {
   return (
-    <ul className="">
+    <ul className="game-nav-list">
       {storeData.map((store) => (
         <li key={store.basePath}>
           <NavLink
             to={`/store/${store.basePath}`}
-            className={({ isActive }) => isActive ? "active" : ""}
+            className="game-nav-link"
           >
             {store.game}
           </NavLink>
