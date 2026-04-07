8dfbb6a1b6d6cda6365ccdb67fd3b59f0d3e28a1
JhonveraDev <seguridad.veracontainer@gmail.com>
2026-04-07 13:14:11 -0500

    feat: update sidebar styles and add item count display

--- src/features/store/components/CategorySidebar.tsx
+++ src/features/store/components/CategorySidebar.tsx
@@ -1,21 +1,33 @@
 import { NavLink, useParams } from "react-router-dom";
 import { useCurrentGame } from "../";
+import dot from "../../../assets/images/global/dot_title.svg";
 
 export function CategorySidebar() {
-  const { game } = useParams();
+  const { game, category } = useParams();
   const currentGame = useCurrentGame();
+  const currentCategory = currentGame?.categories.find(c => c.path === category);
 
   if (!currentGame) return null;
 
   return (
     <aside className="sidebar">
+
+      <div className="sidebar__dot">
+        <img src={dot} alt="" />
+        <h2>{currentGame.game}</h2>
+      </div>
+
+      <div className="sidebar__total-items">
+        <p>{currentCategory?.products.length} ITEMS</p>
+      </div>
+
       {currentGame.categories.map(cat => (
         <NavLink
           key={cat.path}
           to={`/store/${game}/${cat.path}`}
-          className={({ isActive }) => isActive ? "active" : ""}
+          className={({ isActive }) => `sidebar__link ${isActive ? "sidebar__link--active" : ""}`}
         >
-          {cat.name}
+          <span>{cat.name}</span>
         </NavLink>
       ))}
     </aside>
