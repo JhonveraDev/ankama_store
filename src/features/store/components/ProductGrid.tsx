82413eccb343d87d67e4624957e277456da1612d
JhonveraDev <seguridad.veracontainer@gmail.com>
2026-04-07 11:57:30 -0500

    chore: clean up code structure and remove unused code blocks

--- src/features/store/components/ProductGrid.tsx
+++ src/features/store/components/ProductGrid.tsx
@@ -8,6 +8,7 @@ export const ProductGrid = () => {
   const currentGame = useCurrentGame();
   const search = searchParams.get("search");
   const currentCategory = currentGame?.categories.find(c => c.path === category);
+  const categorieImage = currentGame?.categorieImage;
 
   if (!currentCategory) return <p>No products</p>;
 
@@ -21,18 +22,28 @@ export const ProductGrid = () => {
   return (
     <div className="product-grid">
       {products.length === 0 ? (
-        <p>No results found</p>
+        <p className="product-grid__empty">No results found</p>
       ) : (
         products.map(product => (
           <Link
             to={`/store/${game}/${category}/${product.slug}`}
             key={product.id}
+            className="product-grid__link"
           >
-            <div>
-              <img src={product.image} alt={product.name} />
-              <h3>{product.name}</h3>
-              <p>${product.price}</p>
-            </div>
+            <article className="product-card">
+              <img
+                className="product-card__image"
+                src={product.image}
+                alt={product.name}
+              />
+              <div className="product-card__name">
+                <img src={categorieImage} alt={currentGame?.game} />
+                <h3>{product.name}</h3>
+              </div>
+              <div className="product-card__price">
+                <p>${product.price}</p>
+              </div>
+            </article>
           </Link>
         ))
       )}
--- src/shared/data/store.data.ts
+++ src/shared/data/store.data.ts
@@ -1,6 +1,6 @@
 import type { StoreData } from "../types";
 
-import dofus from "../../assets/images/categories/nav_item_dofus.png";
+import dofus from "../../assets/images/products/dofus.22848db4.svg";
 import wakfu from "../../assets/images/categories/nav_item_wakfu.png";
 
 import weekly from "../../assets/images/banners/banner_dofus_weekly_deals.jpg";
@@ -13,6 +13,8 @@ import prueba2 from "../../assets/images/banners/prueba-2.jpg";
 import prueba22 from "../../assets/images/banners/prueba-22.jpg";
 import prueba13 from "../../assets/images/banners/prueba-13.jpg";
 
+import thumb1 from "../../assets/images/products/original_29460_all.w368h368.png";
+
 
 export const storeData: StoreData = [
   {
@@ -24,25 +26,81 @@ export const storeData: StoreData = [
         name: "Weekly Deals",
         path: "weekly-deals",
         banners: [
-          { mainBanner: weekly, thumbBanner: weeklys, link: "/store/dofus/weekly-deals"},
-          { mainBanner: prueba13, thumbBanner: weeklys, link: "/store/dofus/mounts"},
+          { mainBanner: weekly, thumbBanner: weeklys, link: "/store/dofus/weekly-deals" },
+          { mainBanner: prueba13, thumbBanner: weeklys, link: "/store/dofus/mounts" },
         ],
         products: [
           {
             id: 1,
             slug: "haven-bag-showroom",
-            name: "Haven Bag Showroom",
-            image: "/images/haven-bag.jpg",
+            name: "Haven Bag Showroom adasdasdas",
+            image: thumb1,
             price: 5640,
             originalPrice: 9400,
             discount: 40,
           },
           {
             id: 2,
-            slug: "golden-pack",
-            name: "Golden Pack",
-            image: "/images/golden-pack.jpg",
-            price: 2000,
+            slug: "haven-bag-showroom",
+            name: "Haven Bag Showroom",
+            image: thumb1,
+            price: 5640,
+            originalPrice: 9400,
+            discount: 40,
+          },
+          {
+            id: 3,
+            slug: "haven-bag-showroom",
+            name: "Haven Bag Showroom",
+            image: thumb1,
+            price: 5640,
+            originalPrice: 9400,
+            discount: 40,
+          },
+          {
+            id: 4,
+            slug: "haven-bag-showroom",
+            name: "Haven Bag Showroom",
+            image: thumb1,
+            price: 5640,
+            originalPrice: 9400,
+            discount: 40,
+          },
+          {
+            id: 5,
+            slug: "haven-bag-showroom",
+            name: "Haven Bag Showroom",
+            image: thumb1,
+            price: 5640,
+            originalPrice: 9400,
+            discount: 40,
+          },
+          {
+            id: 6,
+            slug: "haven-bag-showroom",
+            name: "Haven Bag Showroom",
+            image: thumb1,
+            price: 5640,
+            originalPrice: 9400,
+            discount: 40,
+          },
+          {
+            id: 7,
+            slug: "haven-bag-showroom",
+            name: "Haven Bag Showroom",
+            image: thumb1,
+            price: 5640,
+            originalPrice: 9400,
+            discount: 40,
+          },
+          {
+            id: 8,
+            slug: "haven-bag-showroom",
+            name: "Haven Bag Showroom",
+            image: thumb1,
+            price: 5640,
+            originalPrice: 9400,
+            discount: 40,
           },
         ],
       },
