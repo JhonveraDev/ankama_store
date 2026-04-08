236c8c8ee477fa1f0047e50795f8559d70a4a2a4
JhonveraDev <seguridad.veracontainer@gmail.com>
2026-04-08 17:52:14 -0500

    new styles in recentProducts

--- src/features/store/components/RecentProducts.tsx
+++ src/features/store/components/RecentProducts.tsx
@@ -1,23 +1,33 @@
 import { Link } from "react-router-dom";
 import { useRecentProducts } from "../";
+import { useCurrentGame } from "../";
 
 export const RecentProducts = () => {
-  const products = useRecentProducts();
+  const products = useRecentProducts().filter(Boolean);
+  const currentGame = useCurrentGame();
+  const categorieImage = currentGame?.categorieImage;
 
   if (products.length === 0) return null;
 
   return (
     <div className="recent-products">
-      <h2>Recently Viewed</h2>
+      <h2>Recently Viewed Items</h2>
       <div className="recent-products__grid">
         {products.map(product => (
           <Link
             key={product!.slug}
             to={`/store/${product!.game}/${product!.category}/${product!.slug}`}
           >
-            <img src={product!.image} alt={product!.name} />
-            <p>{product!.name}</p>
-            <p>${product!.price}</p>
+            <article className="product-card">
+              <img className="product-card__image" src={product!.image} alt={product!.name} />
+              <div className="product-card__name recent">
+                <img src={categorieImage} alt={currentGame?.game} />
+                <h3>{product!.name}</h3>
+              </div>
+              {/* <div className="product-card__price">
+                <p>${product!.price}</p>
+              </div> */}
+            </article>
           </Link>
         ))}
       </div>
